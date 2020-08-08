d3c7ef3e6f9c0c7386d35db5bb63e8ea5761d282
rockts <rockts@sina.com>
2020-08-08 21:57:49 +0800

    创建作者表单

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,5 @@
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 const Author = require("../models/author");
 var async = require("async");
 var Book = require("../models/book");
@@ -48,15 +50,73 @@ exports.author_detail = function (req, res, next) {
     }
   );
 };
-// 由 GET 显示创建作者的表单
-exports.author_create_get = (req, res) => {
-  res.send("未实现：作者创建表单的 GET");
+
+// Display Author create form on GET.
+exports.author_create_get = function (req, res, next) {
+  res.render("author_form", { title: "Create Author" });
 };
 
-// 由 POST 处理作者创建操作
-exports.author_create_post = (req, res) => {
-  res.send("未实现：创建作者的 POST");
-};
+// Handle Author create on POST.
+exports.author_create_post = [
+  // Validate fields.
+  body("first_name")
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+
+  // Sanitize fields.
+  sanitizeBody("first_name").trim().escape(),
+  sanitizeBody("family_name").trim().escape(),
+  sanitizeBody("date_of_birth").toDate(),
+  sanitizeBody("date_of_death").toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create an Author object with escaped and trimmed data.
+      var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new author record.
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 // 由 GET 显示删除作者的表单
 exports.author_delete_get = (req, res) => {
