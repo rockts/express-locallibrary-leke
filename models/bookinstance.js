71231143fdda049130f47744738d23f5bcf1c1a5
rockts <rockts@sina.com>
2020-08-08 21:32:50 +0800

    创建图书副本详情页

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,12 +1,10 @@
+var mongoose = require("mongoose");
 var moment = require("moment");
-const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
-const BookInstanceSchema = new Schema({
-  // 指向相关藏书的引用
-  book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
-  // 出版项
+var BookInstanceSchema = new Schema({
+  book: { type: Schema.ObjectId, ref: "Book", required: true }, // Reference to the associated book.
   imprint: { type: String, required: true },
   status: {
     type: String,
@@ -17,10 +15,18 @@ const BookInstanceSchema = new Schema({
   due_back: { type: Date, default: Date.now },
 });
 
-// 虚拟属性'url'：藏书副本 URL
+// Virtual for this bookinstance object's URL.
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/bookinstances/" + this._id;
+});
+
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return moment(this.due_back).format("MMMM Do, YYYY");
 });
 
-// 导出 BookInstancec 模型
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return moment(this.due_back).format("YYYY-MM-DD");
+});
+
+// Export model.
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
