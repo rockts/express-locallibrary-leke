d8a77b42af0dc8914f3e735435fc72cd18e35bea
rockts <rockts@sina.com>
2020-08-08 21:48:31 +0800

    创建类型表单

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,8 @@
 var Genre = require("../models/genre");
 var Book = require("../models/book");
 var async = require("async");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
@@ -50,15 +52,59 @@ exports.genre_detail = function (req, res, next) {
   );
 };
 
-// 由 GET 显示创建作者的表单
-exports.genre_create_get = (req, res) => {
-  res.send("未实现：作者创建表单的 GET");
+// Display Genre create form on GET.
+exports.genre_create_get = function (req, res, next) {
+  res.render("genre_form", { title: "Create Genre" });
 };
 
-// 由 POST 处理作者创建操作
-exports.genre_create_post = (req, res) => {
-  res.send("未实现：创建作者的 POST");
-};
+// Handle Genre create on POST.
+exports.genre_create_post = [
+  // Validate that the name field is not empty.
+  body("name", "Genre name required").isLength({ min: 1 }).trim(),
+
+  // Sanitize (trim and escape) the name field.
+  sanitizeBody("name").trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec(function (err, found_genre) {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url);
+        } else {
+          genre.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(genre.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // 由 GET 显示删除作者的表单
 exports.genre_delete_get = (req, res) => {
