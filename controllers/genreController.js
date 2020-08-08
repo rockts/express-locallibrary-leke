b27d2e275947fabc1809fca3943d18ca66a33ae3
rockts <rockts@sina.com>
2020-08-08 22:39:41 +0800

    添加种类删除

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -106,14 +106,71 @@ exports.genre_create_post = [
   },
 ];
 
-// 由 GET 显示删除作者的表单
-exports.genre_delete_get = (req, res) => {
-  res.send("未实现：作者删除表单的 GET");
+// Display Genre delete form on GET.
+exports.genre_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        res.redirect("/catalog/genres");
+      }
+      // Successful, so render.
+      res.render("genre_delete", {
+        title: "Delete Genre",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
-// 由 POST 处理作者删除操作
-exports.genre_delete_post = (req, res) => {
-  res.send("未实现：删除作者的 POST");
+// Handle Genre delete on POST.
+exports.genre_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      if (results.genre_books.length > 0) {
+        // Genre has books. Render in same way as for GET route.
+        res.render("genre_delete", {
+          title: "Delete Genre",
+          genre: results.genre,
+          genre_books: results.genre_books,
+        });
+        return;
+      } else {
+        // Genre has no books. Delete object and redirect to the list of genres.
+        Genre.findByIdAndRemove(req.body.id, function deleteGenre(err) {
+          if (err) {
+            return next(err);
+          }
+          // Success - go to genres list.
+          res.redirect("/catalog/genres");
+        });
+      }
+    }
+  );
 };
 
 // 由 GET 显示更新作者的表单
